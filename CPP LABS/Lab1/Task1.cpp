ea3fb68d3896b18bdc3eb0f80dced4db56e387c1
RAGHIBRIZWAN <142868123+RAGHIBRIZWAN@users.noreply.github.com>
2024-01-28 15:02:25 +0500

    Update Task1.cpp

--- CPP LABS/Lab1/Task1.cpp
+++ CPP LABS/Lab1/Task1.cpp
@@ -1,17 +1,23 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+int main() {
     int num;
-    cout<<"Enter a number: "<<endl;
-    cin>> num;
-    for (int i = 2; i < num/2; i++)
-    {
-        if(num % i == 0){
-        cout<<"It is not a prime number"<<endl;
-        }
+    cout << "Enter a number: " << endl;
+    cin >> num;
+
+    if (num < 2) {
+        cout << "Not a prime number" << endl;
         return 0;
     }
-    cout<<"It is  a prime number"<<endl;
-    
+
+    for (int i = 2; i <= num / 2; i++) {
+        if (num % i == 0) {
+            cout << "It is not a prime number" << endl;
+            return 0;
+        }
+    }
+    cout << "It is a prime number" << endl;
+
+    return 0;
 }
