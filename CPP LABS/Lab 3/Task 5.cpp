e6a0f9cd566d5c43b60b0690f6bb007c82d4b979
RAGHIBRIZWAN <142868123+RAGHIBRIZWAN@users.noreply.github.com>
2024-02-09 23:47:03 +0500

    Update Task 5.cpp

--- CPP LABS/Lab 3/Task 5.cpp
+++ CPP LABS/Lab 3/Task 5.cpp
@@ -18,8 +18,8 @@ public:
     {
         for (int i = 0; i < sell.size(); i++)
         {
-                cout << "Item is: " << sell[i] << endl;
-                cout << "Price is: " << price[i] << endl;
+            cout << "Item is: " << sell[i] << endl;
+            cout << "Price is: " << price[i] << endl;
         }
     }
 
@@ -38,21 +38,89 @@ public:
     {
         for (int i = 0; i < sell.size(); i++)
         {
-                cout << sell[i] << endl;
+            cout << sell[i] << endl;
         }
+    }
+
+    void receipt()
+    {
+        int bill = 0;
+        int products = 0, quantity = 1;
+        string items;
+
+        cout << "How many products are there?";
+        cin >> products;
+
+        for (int i = 0; i < products; i++)
+        {
+            cout << "Enter product name: ";
+            cin >> items;
+            for (int j = 0; j < products; j++)
+            {
+                if (items == sell[j])
+                {
+                    cout << "Enter quantity of the product: ";
+                    cin >> quantity;
+                    bill += price[j] * quantity;
+                }
+            }
+        }
+
+        cout << "Yout total bill is: " << bill<<endl;
     }
 };
 
 int main()
 {
+    string prod,mod;
+    int cost1,cost2;
     Stationary shop;
-    shop.adds("Bread", 100);
-    shop.adds("Eggs", 50);
-    shop.adds("Butter", 80);
-    cout << "Retrieve function: " << endl;
-    shop.retrieve();
-    cout << "Display function: " << endl;
-    shop.display();
-}
 
-//INCOMPLETE CODE
+    int number=0;
+    while (number!=6)
+{
+    cout << "1.add\n2.modify\n3.retrieve\n4.display\n5.reciept\n6.exit\n";
+    cin >> number;
+
+
+
+        switch (number)
+        {
+        case 1:
+            
+            cout << "What do you want to add?" << endl;
+            cin >> prod;
+            cout << "What is its price?" << endl;
+            cin>>cost1;
+            shop.adds(prod, cost1);
+            break;
+
+            case 2:
+            cout << "What do you want to modify?" << endl;
+            cin >> mod;
+            cout << "What is its price?" << endl;
+            cin>>cost2;
+            shop.modify(mod, cost2);
+            break;
+
+            case 3:
+            shop.retrieve();
+            break;
+
+            case 4:
+            shop.display();
+            break;
+
+            case 5:
+            shop.receipt();
+            break;
+
+            case 6:
+            return 0;
+            break;
+
+        default:
+            break;
+        }
+    }
+}
