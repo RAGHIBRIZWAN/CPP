a842655672e0ac8f05fa2aa486633bde2bc4de2a
RAGHIBRIZWAN <142868123+RAGHIBRIZWAN@users.noreply.github.com>
2024-02-09 10:05:56 +0500

    Create Task 1.cpp

---
+++ CPP LABS/Lab 3/Task 1.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+using namespace std;
+
+class BoardMarker
+{
+    string Brand;
+    string Shade;
+    bool Refillable;
+    bool Status;
+
+public:
+    void setBrand(string brand)
+    {
+        Brand = brand;
+    }
+    void setShade(string shade)
+    {
+        Shade = shade;
+    }
+    void setRefillable(bool refill)
+    {
+        Refillable = refill;
+    }
+    void setStatus(bool status)
+    {
+        Status = status;
+    }
+
+    void writing(){
+        if(Status == false){
+            cout<<"Ink is finished"<<endl;
+        }
+        else{
+            cout<<"You can write more"<<endl;
+        }
+    }
+
+    void refill(){
+        if(Refillable){
+            cout<<"It is refillable"<<endl;
+        }
+        else{
+        cout<<"It is not refillable"<<endl;
+        }
+    }
+
+};
+
+int main()
+{
+    BoardMarker marker;
+    marker.setBrand("Dollor");
+    marker.setShade("Black");
+    marker.setRefillable(true);
+    marker.setStatus(true);
+    marker.writing();
+    marker.refill();
+}
