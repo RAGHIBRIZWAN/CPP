7475d6d57b29e85adb0122457ca2805ad9668c5d
RAGHIBRIZWAN <142868123+RAGHIBRIZWAN@users.noreply.github.com>
2024-02-15 22:23:37 +0500

    Create Task4.cpp

---
+++ CPP LABS/Lab 4/Task4.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+class Office
+{
+    string Location;
+    int seatingCapacity;
+    string furniture[3];
+
+public:
+    Office(string location = "", int capacity = 0, string *furn = nullptr)
+        : Location(location), seatingCapacity(capacity)
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            furniture[i] = furn[i];
+        }
+    }
+
+    void DisplayInfo()
+    {
+        cout << "Location: " << Location << endl;
+        cout << "Seating Capacity: " << seatingCapacity << endl;
+        cout << "Furniture: " << furniture[0] << ", " << furniture[1] << ", " << furniture[2] << endl;
+    }
+};
+
+int main()
+{
+    string furn[3] = {"Desk", "Chair", "Bookshelf"};
+    Office office1("Downtown", 50, furn);
+    cout << "Office 1:" << endl;
+    office1.DisplayInfo();
+    cout << endl;
+}
