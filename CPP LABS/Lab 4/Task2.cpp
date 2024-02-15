e1ce382cccbd84c155027d107db72481cbd1648f
RAGHIBRIZWAN <142868123+RAGHIBRIZWAN@users.noreply.github.com>
2024-02-15 15:36:01 +0500

    Create Task2.cpp

---
+++ CPP LABS/Lab 4/Task2.cpp
@@ -0,0 +1,61 @@
+/*
+Programmer: Raghib Rizwan Rabani
+NU_ID: 23K-0012
+Date: 15/2/2024
+*/
+
+#include<iostream>
+using namespace std;
+
+class Book{
+    string name;
+    string author;
+    string ISBN;
+    int pageCount;
+    int pagesread;
+public:
+
+    Book(){
+        pageCount = 1000;
+        pagesread = 0;
+    }
+
+    void setvalues(string Name,string Author,string isbn){
+        name = Name;
+        author = Author;
+        ISBN = isbn;
+    }
+
+    void read(){
+        int read;
+        cout<<"How many pages have you read?"<<endl;
+        cin>>read;
+        pagesread += read;
+        if (pagesread == pageCount )
+        {
+            cout<<"You have finished the book"<<endl;
+        }
+        else if(pagesread > pageCount){
+            cout<<"There are not much pages in book";
+        }
+        else{
+            cout<<"You have not finished the book"<<endl;
+        }
+    }
+
+    void showBookInfo(void){
+        cout<<"Pages Count "<<pageCount<<endl;
+        cout<<"Pages read "<<pagesread<<endl;
+        cout<<"Name "<<name<<endl;
+        cout<<"Author "<<author<<endl;
+        cout<<"ISBN "<<ISBN<<endl;
+    }
+};
+
+int main(){
+    Book books;
+    books.setvalues("Harry_Potter","Raghib","0124589637");
+    books.showBookInfo();
+    books.read();
+    books.showBookInfo();
+}
