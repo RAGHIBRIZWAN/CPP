0dc6d3e46d7b05dd0f79bffaa538e3ee22c69f54
RAGHIBRIZWAN <142868123+RAGHIBRIZWAN@users.noreply.github.com>
2024-02-22 23:24:39 +0500

    Create Task 1.cpp

---
+++ CPP LABS/Lab 5/Task 1.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+class Length{
+    float sideLength;
+    float area;
+    static float allAreas;
+    
+public:
+    Length(){};
+    Length(float sideLength){
+        this -> sideLength = sideLength;
+    }
+    void calculateArea(){
+        area = sideLength * sideLength;
+        allAreas += area;
+    }
+    void setdata(float sideLength){
+        this -> sideLength = sideLength;
+    }
+    void getdata(){
+        cout << "Side Length: " << sideLength << endl;
+        cout << "Area: " << area << endl;
+        cout << "Total Area: " << allAreas << endl;
+    }
+};
+
+float Length::allAreas = 0;
+
+int main() {
+    Length side;
+    side = Length(5);
+    side.calculateArea();
+    side.getdata();
+    side = Length(10);
+    side.calculateArea();
+    side.getdata();
+    return 0;
+}
