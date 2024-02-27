89f6494fc1e5499f72dcbe20aac9dc351d78aec7
RAGHIBRIZWAN <142868123+RAGHIBRIZWAN@users.noreply.github.com>
2024-02-27 23:02:02 +0500

    Update Task 7.cpp

--- CPP LABS/Lab 5/Task 7.cpp
+++ CPP LABS/Lab 5/Task 7.cpp
@@ -4,102 +4,113 @@ NU_ID: 23K-0012
 Date: 25/2/2024
 */
 
-// NOT DONE YET
-
 #include <iostream>
 #include <vector>
 using namespace std;
 
-class menuItems {
+class menuItems
+{
 public:
     string foodName;
     double foodPrice;
 };
 
-class Menu {
-private:
-    vector<menuItems> menu;
+class Menu
+{
 
 public:
-    void addItems() {
+    vector<menuItems> menu;
+
+    void addItems()
+    {
         int items;
         string food;
         double price;
         cout << "How many items do you want to add? ";
         cin >> items;
-        for (int i = 0; i < items; i++) {
+        for (int i = 0; i < items; i++)
+        {
             cout << "Item: " << i + 1 << endl;
             cout << "Enter food name: ";
             cin >> food;
             cout << "Enter food price: ";
             cin >> price;
-            menuItems item = { food, price };
+            menuItems item = {food, price};
             menu.push_back(item);
         }
     }
 
-    void removes() {
+    void removes()
+    {
         string food;
         cout << "Enter the name of the food to remove: ";
         cin >> food;
 
         bool itemRemoved = false;
-        for (size_t i = 0; i < menu.size(); i++) {
-            if (menu[i].foodName == food) {
+        for (int i = 0; i < menu.size(); i++)
+        {
+            if (menu[i].foodName == food)
+            {
                 menu.erase(menu.begin() + i);
                 itemRemoved = true;
                 break;
             }
         }
 
-        if (!itemRemoved) {
+        if (!itemRemoved)
+        {
             cout << "Item not found." << endl;
         }
     }
 
-    void display() {
-        for (int i = 0; i < menu.size(); i++) {
+    void display()
+    {
+        for (int i = 0; i < menu.size(); i++)
+        {
             cout << menu[i].foodName << "  " << menu[i].foodPrice << endl;
         }
     }
-
-    vector<menuItems>& getMenu() {
-        return menu;
-    }
 };
 
-class Payment {
+class Payment
+{
 public:
     double amount;
 };
 
-class Order {
-private:
+class Order
+{
+
+public:
     vector<menuItems> items;
     double total;
 
-public:
-    void add(menuItems item) {
+    void add(menuItems item)
+    {
         items.push_back(item);
         total += item.foodPrice;
     }
 
-    double getTotal() {
+    double getTotal()
+    {
         return total;
     }
 };
 
-int main() {
+int main()
+{
     Menu menu;
     Order order;
     int choice;
 
-    do {
+    do
+    {
         cout << "\n1. Add item to menu\n2. Remove item from menu\n3. Display menu\n4. Place order\n5. Exit\n";
         cout << "Enter your choice: ";
         cin >> choice;
 
-        switch (choice) {
+        switch (choice)
+        {
         case 1:
             menu.addItems();
             break;
@@ -110,28 +121,34 @@ int main() {
             menu.display();
             break;
         case 4:
+        {
+            int items;
+            cout << "How many items do you want to order? ";
+            cin >> items;
+            for (int i = 0; i < items; i++)
             {
-                int items;
-                cout << "How many items do you want to order? ";
-                cin >> items;
-                for (int i = 0; i < items; i++) {
-                    string food;
-                    cout << "Name the item " << i + 1 << ": ";
-                    cin >> food;
-                    for (int j = 0; j < menu.getMenu().size(); j++) {
-                        if (menu.getMenu()[j].foodName == food) {
-                            order.add(menu.getMenu()[j]);
-                            break;
-                        }
+                string food;
+                cout << "Name the item " << i + 1 << ": ";
+                cin >> food;
+                for (int j = 0; j < menu.menu.size(); j++)
+                {
+                    if (menu.menu[j].foodName == food)
+                    {
+                        order.add(menu.menu[j]);
+                        break;
                     }
                 }
-                cout << "Total cost: " << order.getTotal() << endl;
             }
-            break;
+            cout << "Total cost: " << order.getTotal() << endl;
+        }
+
+        break;
         default:
             break;
         }
-    } while (choice != 5);
+        }
+        while (choice != 5)
+            ;
 
-    return 0;
-}
+        return 0;
+    }
