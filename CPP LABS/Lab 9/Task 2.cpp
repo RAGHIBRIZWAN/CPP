b44f43ae0ff49b3b1da19618a4babcf739b0e101
RAGHIBRIZWAN <142868123+RAGHIBRIZWAN@users.noreply.github.com>
2024-03-28 20:39:27 +0500

    Update Task 2.cpp

--- CPP LABS/Lab 9/Task 2.cpp
+++ CPP LABS/Lab 9/Task 2.cpp
@@ -5,6 +5,7 @@ double pi = 3.142;
 
 class Shape{
 public:
+    Shape(){}
     virtual double area() const{
         return 0;
     }
@@ -18,17 +19,14 @@ public:
 class Circle:public Shape{
     double r;
 public:
-    // Circle(double r){
-    //     this->r = r;
-    // }
+    Circle(){}
+    Circle(double r):r(r){}
     double area(){
-        cout<<"Enter the value of the radius: "<<endl;
-        cin>>this->r;
-        cout<<"The area of triangle is: ";
+        cout<<"The area of circle is: ";
         return pi*r*r;
     }
     double perimeter(){
-        cout<<"The perimeter of triangle is: ";
+        cout<<"The perimeter of circle is: ";
         return 2*pi*r;
     }
     void displayProperties(){
@@ -42,11 +40,9 @@ class Rectangle:public Shape{
     double L;
     double W;
 public:
-     double area(){
-        cout<<"Enter the value of the length: "<<endl;
-        cin>>this->L;
-        cout<<"Enter the value of the width: "<<endl;
-        cin>>this->W;
+    Rectangle(){}
+    Rectangle(double L,double W):L(L),W(W){}
+    double area(){
         cout<<"The area of rectangle is: ";
         return L*W;
     }
@@ -63,10 +59,10 @@ public:
 class Square:public Shape{
     double L;
 public:
-     double area(){
-        cout<<"Enter the value of the length: "<<endl;
-        cin>>this->L;
-        cout<<"The area of rectangle is: ";
+    Square(){}
+    Square(double L):L(L){}
+    double area(){
+        cout<<"The area of square is: ";
         return L*L;
     }
     double perimeter(){
@@ -85,17 +81,13 @@ protected:
     double H;
     double S;
 public:
+    Triangle(){}
+    Triangle(double B,double H,double S):B(B),H(H),S(S){}
     double perimeter(){
         cout<<"The perimeter of rectangle is: ";
         return B + H + S;
     }
     double area(){
-        cout<<"Enter the value of the base: "<<endl;
-        cin>>this->B;
-        cout<<"Enter the value of the height: "<<endl;
-        cin>>this->H;
-         cout<<"Enter the value of the side: "<<endl;
-        cin>>this->S;
         cout<<"The area of triangle is: ";
         return sqrt(perimeter()*(perimeter()-B)*(perimeter()-H)*(perimeter()-S));
     }
@@ -105,45 +97,83 @@ public:
     }
 };
 class Equilateraltriangle:public Triangle{
-     double area(){
-        cout<<"Enter the value of the height: "<<endl;
-        cout<<"The area of triangle is: ";
-        return B*B*B;
+public:
+    Equilateraltriangle(){}
+    Equilateraltriangle(double B):Triangle(B,B,B){}
+    double area(){
+        cout<<"The area of equilateral triangle is: ";
+        return B * B * B;
     }
     void displayProperties(){
         cout<<"AREA: "<<area()<<endl;
     }
-} ;
+};
 
 int main(){
+    int choice;
+    cout<<"Welcome to the Geometry Competition Calculator! "<<endl;
+    cout<<endl;
     Shape *shp;
-    Circle cir;
-    shp = &cir;
-    shp->area();
-    shp->perimeter();
-    shp->displayProperties();
-    cout<<endl;
-    Square sqr;
-    shp = &sqr;
-    shp->area();
-    shp->perimeter();
-    shp->displayProperties();
-    cout<<endl;
-    Rectangle rec;
-    shp = &rec;
-    shp->area();
-    shp->perimeter();
-    shp->displayProperties();
-    cout<<endl;
-    Triangle tri;
-    shp = &tri;
-    shp->area();
-    shp->perimeter();
-    shp->displayProperties();
-    cout<<endl;
-    Equilateraltriangle equ;
-    shp = &equ;
-    shp->area();
-    shp->perimeter();
-    shp->displayProperties();
+    do{
+        cout<<"Please select a shape: "<<endl;
+        cout<<"1.Circle\n2.Rectangle\n3.Square\n4.Triangle\n5.Equilateral Triangle\n6.Exit"<<endl;
+        cin>>choice;
+        switch (choice){
+            case 1:{
+                double radius = 5.0;
+                Circle cir(radius);
+                shp = &cir;
+                shp->area();
+                shp->perimeter();
+                shp->displayProperties(); 
+                break;
+            }
+            case 2:{
+                double L=4.0,W=5.0;
+                Rectangle rec(L,W);
+                shp = &rec;
+                shp->area();
+                shp->perimeter();
+                shp->displayProperties(); 
+                break;
+            }
+            case 3:{
+                double L=4.0;
+                Square sqr(L);
+                shp = &sqr;
+                shp->area();
+                shp->perimeter();
+                shp->displayProperties(); 
+                break;
+            }
+            case 4:{
+                double B = 4.0, H = 5.0, S = 3.0;
+                Triangle tri(B,H,S);
+                shp = &tri;
+                shp->area();
+                shp->perimeter();
+                shp->displayProperties(); 
+                break;
+            }
+            case 5:{
+                double B = 4.0;
+                Equilateraltriangle equi(B);
+                shp = &equi;
+                shp->area();
+                shp->perimeter();
+                shp->displayProperties(); 
+                break;
+            }
+            case 6:
+                return 0;
+            default:
+                break;
+        }
+        string cont;
+        cout<<"Do you want to continue? (yes/no) ";
+        cin>>cont;
+        if (cont != "yes"){
+            return 0;
+        }
+    }while(1);
 }
