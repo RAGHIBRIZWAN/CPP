56ca502b70bae59f26ce8bd524272c34c7472e76
RAGHIBRIZWAN <142868123+RAGHIBRIZWAN@users.noreply.github.com>
2024-04-30 01:02:34 +0500

    Create Task1.cpp

---
+++ CPP LABS/Lab 11/Task1.cpp
@@ -0,0 +1,44 @@
+/*
+Programmer: Raghib Rizwan Rabani
+NU ID: 23K-0012
+*/
+
+#include<iostream>
+using namespace std;
+
+class Convert{
+protected:
+    float val1,val2;
+public:
+    Convert(float val1,float val2):val1(val1),val2(val2){}
+    virtual void compute(){}
+};
+
+class l_to_g : public Convert{
+public:
+    l_to_g(float val1):Convert(val1, 0){}
+    void compute() override {
+        val2 = val1 * 0.264172;
+        cout<<val2<<endl;
+    }
+};
+
+class f_to_c : public Convert{
+public:
+    f_to_c(float val1):Convert(val1, 0){}
+    void compute() override {
+        val2 = (val1 - 32) * (5.0/9.0);
+        cout<<val2<<endl;
+    }
+};
+
+int main(){
+    l_to_g l(4);
+    f_to_c f(70);
+
+    Convert* con1 = &l;
+    Convert* con2 = &f;
+
+    con1->compute();
+    con2->compute();
+}
