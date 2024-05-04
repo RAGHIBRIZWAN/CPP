53abc53d1c6514c6b881ef491fceb2409cbc80f0
RAGHIBRIZWAN <142868123+RAGHIBRIZWAN@users.noreply.github.com>
2024-05-04 13:52:07 +0500

    Update Question2.cpp

--- Assignment 3/Question2.cpp
+++ Assignment 3/Question2.cpp
@@ -3,10 +3,10 @@ using namespace std;
 
 template <typename T>
 class Pet{
-    string name;
-    int age;
+    T name;
+    int age; // Change age to int
 public:
-    Pet(string name, int age):name(name),age(age){}
+    Pet(T name, int age):name(name),age(age){}
 
     virtual void makeSound() = 0;
 
@@ -16,41 +16,50 @@ public:
     }
 };
 
-class Cat : public Pet<Cat>{
+template <typename T>
+class Cat : public Pet<T>{
 public:
-    Cat(string name, int age):Pet(name,age){}
+    Cat(T name, int age):Pet<T>(name,age){}
 
     void makeSound(){
-        Print();
         cout<<"Meow"<<endl;
     }
 };
 
-class Dog : public Pet<Dog>{
+template <typename T>
+class Dog : public Pet<T>{
 public:
-    Dog(string name, int age):Pet(name,age){}
+    Dog(T name, int age):Pet<T>(name,age){}
 
     void makeSound(){
-        Print();
         cout<<"Woof"<<endl;
     }
 };
 
-class Bird : public Pet<Bird>{
+template <typename T>
+class Bird : public Pet<T>{
 public:
-    Bird(string name, int age):Pet(name,age){}
+    Bird(T name, int age):Pet<T>(name,age){}
 
     void makeSound(){
-        Print();
         cout<<"Chirp"<<endl;
     }
 };
 
 int main(){
-    Cat cat("Lulu",1);
-    cat.makeSound();
-    Dog dog("Tommy",2);
-    dog.makeSound();
-    Bird bird("Fiffy",3);
-    bird.makeSound();
+    Pet<string>* pet;
+    Cat<string> cat("Lulu",1);
+    Dog<string> dog("Tommy",2);
+    Bird<string> bird("Fiffy",3);
+    pet = &cat;
+    pet->makeSound();
+    pet->Print();
+    cout<<endl;
+    pet = &dog;
+    pet->makeSound();
+    pet->Print();
+    cout<<endl;
+    pet = &bird;
+    pet->makeSound();
+    pet->Print(); 
 }
